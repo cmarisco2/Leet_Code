6209f1c9ec40ca2ff6e7fc9fe152480031502b90
Christopher Marisco <christopher.marisco@gmail.com>
2022-01-26 09:37:13 -0700

    add comments to java solution

--- Arrays/Replace_Right_Elements.java
+++ Arrays/Replace_Right_Elements.java
@@ -1,3 +1,20 @@
+package Arrays;
+//? LeetCode problem 1299 - 'Easy'
+
+// Problem Statement:
+// Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1
+
+// After doing so, return the array
+
+/**
+ * Input array: [17, 18, 5, 4, 6, 1]
+ * Output: [18, 6, 6, 6, 1, -1]
+ */
+
+// ? SOLUTION Notes:
+// * 1) Reverse Iterate
+// * 2) oldMax = -1 (before loop)
+// * 3) newMax = max(oldMax, arr[i])
 class Replace_Right_Elements {
     public static int[] replaceElements(int[] arr) {
         int oldMax = -1;
