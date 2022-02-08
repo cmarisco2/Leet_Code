ff9e67c58926adc72406a600688d1b430933225e
Christopher Marisco <christopher.marisco@gmail.com>
2022-02-08 11:36:24 -0700

    Add Comments for Problem Statement, Examples, Solution Breakdown, and Time/Space Complexity

--- Sliding_Window/MinimumSizeSubarraySum_209.java
+++ Sliding_Window/MinimumSizeSubarraySum_209.java
@@ -1,3 +1,33 @@
+//? Leetcode Problem 209: Minimum Size Subarray Sum
+//? Problem Statement:
+// Given an array of positive integers nums and a positive integer target, return the minimal length of a contiguous subarray [numsl, numsl+1, ..., numsr-1, numsr] of which the sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+//? Examples:
+// Input: target = 7, nums = [2,3,1,2,4,3]
+// Output: 2
+// Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+
+// Input: target = 4, nums = [1,4,4]
+// Output: 1
+
+// Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+// Output: 0
+
+//? Solution Notes:
+//! KEY - 'Minimum Size' + 'Subarray/Substring' + 'Target/Sum' = SLIDING WINDOW
+
+//* 1) 2Ptrs i & j, Both Start at 0, j moving left to right.
+//* 2) Add j's value to sum. j increments with loop.
+//* 3) while(sum>=target) -> remove i's value. increment i. update min length.
+    //* --min length is initially high constant.
+    //* --if i == j, sum is value at i or j.
+    //* --if min length is never updated, return 0. else return min length.
+
+//? Complexity:
+//* TC -> O(N)
+    //* Iterate through the Loop Once.
+//* SC -> O(1)
+    //* Use only i,j,sum, and minLength regardless of size of nums[]
 package Sliding_Window;
 class MinimumSizeSubarraySum_209{
     public static int minSubArrayLen(int target, int[] nums) {
@@ -17,6 +47,6 @@ class MinimumSizeSubarraySum_209{
     }
 
     public static void main(String[] args){
-        
+        int[] nums = {2, 3, 1, 2, 4, 3};
     }
 }
