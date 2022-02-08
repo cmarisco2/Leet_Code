b59837336eeb24a02de266fe9318a22224e005d2
Christopher Marisco <christopher.marisco@gmail.com>
2022-02-07 18:22:47 -0700

    Add Problem Statement, Examples, and Solution Approach to the Solution

--- Sliding_Window/LongestSubstringNoRepeatedChars_3.java
+++ Sliding_Window/LongestSubstringNoRepeatedChars_3.java
@@ -1,3 +1,24 @@
+//? Leetcode Problem 3: Longest Substring Without Repeating Characters
+// Given a string s, find the length of the longest substring without repeating characters.
+
+//? Examples:
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+//?Solution Notes: SLIDING WINDOW APPROACH:
+//* 1) Use HashSet 
+//* 2) Check & Add Elements to SET from the RIGHT.
+//* 3) IF Repeat -> Remove From LEFT Until No Repeats Exist in SET
 package Sliding_Window;
 import java.util.HashSet;
 
