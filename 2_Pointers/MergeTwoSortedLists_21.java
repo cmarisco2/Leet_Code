53c13de678ae25a4f57c2a1f60924e01f9d328a9
Christopher Marisco <christopher.marisco@gmail.com>
2022-02-14 16:22:59 -0700

    Solve Leetcode Problem #21: Merge Two Sorted Lists

--- 2_Pointers/MergeTwoSortedLists_21.java
+++ 2_Pointers/MergeTwoSortedLists_21.java
@@ -1,15 +1,43 @@
+//? Leetcode Problem 21: Merge Two Sorted Lists
+//? Difficulty: Easy
+
+// You are given the heads of two sorted linked lists list1 and list2.
+
+// Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+// Return the head of the merged linked list.
+
+//? Examples: 
+// List1 = 1 -> 3 -> 4
+// List2 = 1 -> 2 -> 4
+
+// O/P: 1 -> 1 -> 2 -> 3 -> 4 -> 4
+
+//? Solution Notes: (Same subproblem as merge sort except with linked lists)
+//? 2 Ptr Approach (given 2 sorted linear structures)
+//* 1) Create 2 new ptrs to the front of the list w/ smallest value
+    //* Head, 1st ptr above, will remain at front of the list.
+//* 2) Tail equals Head initially, then selects the lesser of the 2 lists vals as its next node
+//* 3) Continue until 1 list is exhausted
+//* 4) Remaining list appended to Tail. Return Head.
+
+
+//? TC -> O(N), SC -> O(1) as no new list is created. Just 2 pointers.
 class MergeTwoSortedLists_21{
 
       public class ListNode {
         int val;
-         ListNode next;
-         ListNode() {}
-         ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { 
+             this.val = val; this.next = next;
+        }
       }
      
 
         public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
             // edge cases:
             if (list1 == null && list2 == null)
                 return null;
@@ -17,6 +45,7 @@ class MergeTwoSortedLists_21{
                 return list2;
             if (list2 == null)
                 return list1;
+
             // 2 Pointers
             ListNode head;
             ListNode tail;
@@ -54,6 +83,7 @@ class MergeTwoSortedLists_21{
                 tail.next = list1;
             while (tail.next != null)
                 tail = tail.next;
+
             // Return list
             return head;
         }
