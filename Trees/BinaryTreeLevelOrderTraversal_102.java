8a3a44af3eda5d2fc3f1b2bcbd65668f487ed5e7
Christopher Marisco <christopher.marisco@gmail.com>
2022-02-24 11:44:53 -0700

    create new directory Trees/ and begin problem 102 Binary Tree Level Order Traversal

---
+++ Trees/BinaryTreeLevelOrderTraversal_102.java
@@ -0,0 +1,24 @@
+//? LeetCode Problem 102: Binary Tree Level Order Traversal
+// Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+//? Definition of Binary Tree:
+
+/**
+ ** Definition for a binary tree node.
+ ** public class TreeNode {
+    ** int val;
+    ** TreeNode left;
+    ** TreeNode right;
+    ** TreeNode() {}
+    ** TreeNode(int val) { this.val = val; }
+    ** TreeNode(int val, TreeNode left, TreeNode right) {
+        ** this.val = val;
+        ** this.left = left;
+        ** this.right = right;
+    ** }
+ ** }
+ */
+
+class BinaryTreeLevelOrderTraversal_102{
+
+}
