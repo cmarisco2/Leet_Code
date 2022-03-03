154fe556ed9d000cba1061b289ffc973934952b0
Christopher Marisco <christopher.marisco@gmail.com>
2022-03-03 16:39:24 -0700

    solve invert binary tree problem

---
+++ Trees/InvertBinaryTree_226.java
@@ -0,0 +1,12 @@
+class InvertBinaryTree_226{
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return null;
+        invertTree(root.left);
+        invertTree(root.right);
+        TreeNode temp = root.right;
+        root.right = root.left;
+        root.left = temp;
+        return root;
+    }
+}
