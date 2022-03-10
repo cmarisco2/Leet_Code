d8f67e8b650d467b4e2736c93eb0e218f48c5c70
Christopher Marisco <christopher.marisco@gmail.com>
2022-03-09 17:18:54 -0700

    add solution notes

--- String/ValidAnagram_242.java
+++ String/ValidAnagram_242.java
@@ -12,6 +12,19 @@
 // Input: s = "rat", t = "car"
 // Output: false
 
+//? Solution Idea: Map Freq Counter
+//* Create 2 Maps that count the frequency of each char element and return true if and only if the count for each character matches.
+//* otherwise, return false
+
+//? Better Idea: Single Radix-Array Counter
+//* create int[] array length of radix (26)
+//* increment the mapped index for each appearance in string S.
+//* decrement the mapped index for each appearance in String T.
+//* Loop through and check for any non-0's.
+
+//? Time Complexity is linear O(N + M), where N = s.length() and M = t.length();
+
+//? Space Complexity is constant as the radix array is of fixed length (26) and doesn't vary with the size of the input string.
 class ValidAnagram_242{
     public boolean isAnagram(String s, String t) {
         int[] alphabet = new int[26];
