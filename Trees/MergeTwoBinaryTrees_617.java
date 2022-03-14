2a16aacc0a1bc34bfa27c5a1fa7d6e08dac1a9a3
Christopher Marisco <christopher.marisco@gmail.com>
2022-03-14 16:00:49 -0600

    add solution notes

--- Trees/MergeTwoBinaryTrees_617.java
+++ Trees/MergeTwoBinaryTrees_617.java
@@ -1,3 +1,24 @@
+//? Leetcode Problem 617: 'Merge Two Binary Trees'
+//? Difficulty: 'Easy'
+
+// You are given two binary trees root1 and root2.
+
+// Imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not. You need to merge the two trees into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of the new tree.
+
+// Return the merged tree.
+
+// Note: The merging process must start from the root nodes of both trees.
+
+//? Solution Approach: Parallel Recursion - Top Down
+//! Key - Ternary Operator For Assignment
+    //* Parallel Tree Traversal Presents an Interesting "Null" Problem
+    //* if 1 node is null and the other isn't, fine at that level.
+        //* BUT the recurse call dfs(node.left) WON'T work if node == NULL
+        //* Need to check and pass null via ternary if that's the case 
+
+
+//? Time & Space:
+//* TC -> O(Max(N,M)): visit each node once simultaneously. Time depends on the
 class MergeTwoBinaryTrees_617{
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
         // Base Case
