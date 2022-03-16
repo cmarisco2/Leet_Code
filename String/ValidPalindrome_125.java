a2ee1d5a7f53d4266df2e57208b4023a47280c67
Christopher Marisco <christopher.marisco@gmail.com>
2022-03-16 15:59:16 -0600

    Add Solution Notes

--- String/ValidPalindrome_125.java
+++ String/ValidPalindrome_125.java
@@ -1,3 +1,38 @@
+//? Leetcode Problem #125: 'Valid Palindrome'
+//? Difficulty: 'Easy'
+//? Description:
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+//? Examples: 
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+
+// Input: s = "race a car"
+// Output: false
+// Explanation: "raceacar" is not a palindrome.
+
+// Input: s = " "
+// Output: true
+// Explanation: s is an empty string "" after removing non-alphanumeric characters.
+// Since an empty string reads the same forward and backward, it is a palindrome.
+
+//? Good Solution:
+//* Create String t.
+//* Add to string t all chars of String s, IFF Character.isLetterOrDigit(char) passes.
+//* Compare the string forwards and backwards (lastChar(t) == firstChar(s))
+//? Solution is TC -> O(N), SC -> O(N)
+
+//? Optimal Solution: 
+//* create filter helper function 'isAlphaNumeric' to test ASCII values of chars.
+//* Use 2 Pointers in primary function. left = start of s, right = end of s.
+//* While(left < right), traverse string
+    //* skip non-alphanumeric chars
+    //* compare lower-case alphanumeric chars
+//? Solution is TC -> O(N), SC -> O(1)
+
 class ValidPalindrome_125{
     public boolean isPalindrome(String s) {
         int left = 0, right = s.length() - 1;
