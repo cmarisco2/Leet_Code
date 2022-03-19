b81706f4d538ef2ade924fa27d2fde2ea52102ea
Christopher Marisco <christopher.marisco@gmail.com>
2022-03-19 14:49:35 -0600

    Add Solution Notes

--- Trees/ValidateBST_98.java
+++ Trees/ValidateBST_98.java
@@ -1,4 +1,37 @@
+//? Leetcode Problem #98: Validate Binary Search Tree
+//? Difficulty: 'Medium'
+//? Description:
+// Given the root of a binary tree, determine if it is a valid binary search tree (BST).
 
+// A valid BST is defined as follows:
+
+// --The left subtree of a node contains only nodes with keys less than the node's key.
+// --The right subtree of a node contains only nodes with keys greater than the node's key.
+// --Both the left and right subtrees must also be binary search trees.
+
+//? Examples: 
+//          5
+//      1       4
+//          3       6 (3 is leftchild of 4)
+
+// Input: root = [5,1,4,null,null,3,6]
+// Output: false
+// Explanation: The root node's value is 5 but its right child's value is 4
+
+//? Solution Notes:
+//* Key: Update left & right bounds during traversal down tree
+//* LB = LeftBound, RB = RightBound
+
+//? Algorithm Idea
+//* Base Case
+//* Check: LB < X.VAL < RB
+//* Goto Right:
+    //* Update LB
+//* Goto Left:
+    //* Update RB
+
+//? Time Complexity == O(N)
+//? Space Complexity == O(Tree Height)
 class ValidateBST_98{
     public boolean isValidBST(TreeNode root) {
         boolean isValid = true;
