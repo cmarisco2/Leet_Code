316c35901660871d2a97bb23cfd1223c433b2e51
Christopher Marisco <christopher.marisco@gmail.com>
2022-03-19 14:20:39 -0600

    Solve Validate BST

--- Trees/ValidateBST_98.java
+++ Trees/ValidateBST_98.java
@@ -1,4 +1,24 @@
 
 class ValidateBST_98{
+    public boolean isValidBST(TreeNode root) {
+        boolean isValid = true;
+        long leftBoundary = Long.MIN_VALUE;
+        long rightBoundary = Long.MAX_VALUE;
+        isValid = checkValid(root, isValid, leftBoundary, rightBoundary);
+        return isValid;
 
+    }
+
+    public boolean checkValid(TreeNode x, boolean isValid, long lBound, long rBound) {
+        if (x == null)
+            return isValid;
+        if (x.val <= lBound)
+            isValid = false;
+        if (x.val >= rBound)
+            isValid = false;
+        isValid = isValid && checkValid(x.left, isValid, lBound, x.val) && isValid
+                && checkValid(x.right, isValid, x.val, rBound);
+
+        return isValid;
+    }
 }
