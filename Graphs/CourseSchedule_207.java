e17b2d0fc7ab8e4a7b46102f418e690fe404fad3
Christopher Marisco <christopher.marisco@gmail.com>
2022-03-21 16:39:22 -0600

    build graph via hashmap vice array indices

--- Graphs/CourseSchedule_207.java
+++ Graphs/CourseSchedule_207.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+
 //? Leetcode Problem 207: Course Schedule 
 //? Difficulty: 'Medium'
 //? Description: 
@@ -18,5 +21,30 @@
 // To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
 
 class CourseSchedule_207{
-    
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        boolean[] marked = new boolean[numCourses];
+        return true;
+    }
+
+    public static boolean dfsCycle(int numCourses, int[][] prereqs, HashMap adj, boolean[] marked, boolean[] onStack){
+        return true;
+    }
+
+    public static void buildGraph(int numCourses, int[][] prereqs, HashMap adj){
+        for (int[] arr : prereqs) {
+            int key = arr[0];
+            if(!adj.containsKey(key)) adj.put(key, new LinkedList<Integer>());
+            ((LinkedList<Integer>) adj.get(key)).add(arr[1]);
+        }
+    }
+
+    public static void main(String[] args){
+        int numCourses = 5;
+        int [][] prereqs = { { 0, 1 }, { 0, 2 }, { 1, 3 }, { 1, 4 }, { 3, 4 } };
+        HashMap<Integer, LinkedList<Integer>> adj = new HashMap<>();
+        buildGraph(numCourses, prereqs, adj);
+        adj.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        });
+    }
 }
