5818f76f5fe64b50b696dfabd46ba725a5636993
Christopher Marisco <christopher.marisco@gmail.com>
2022-03-31 14:34:03 -0600

    Solve Word Search Algorithm. Can make adjustments to improve efficiency a little, but not much due to the brute force backtracking solution

--- Backtracking/WordSearch_79.java
+++ Backtracking/WordSearch_79.java
@@ -1,3 +1,32 @@
 class WordSearch_79{
-    
+    public boolean exist(char[][] board, String word) {
+        // Initialization of Data
+        int ROWS = board.length;
+        int COLS = board[0].length;
+        boolean[][] positionSet = new boolean[ROWS][COLS];
+
+        //Selects each cell as an origin for the dfs() call.
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
+                if (dfs(r, c, 0, board, word, positionSet, ROWS, COLS))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean dfs(int r, int c, int i, char[][] board, String word, boolean[][] positionSet, int ROWS, int COLS) {
+        if (i == word.length())
+            return true;
+        if (r < 0 || c < 0 || r >= ROWS || c >= COLS || word.charAt(i) != board[r][c] || positionSet[r][c])
+            return false;
+        positionSet[r][c] = true;
+        boolean result = dfs(r + 1, c, i + 1, board, word, positionSet, ROWS, COLS)
+                || dfs(r - 1, c, i + 1, board, word, positionSet, ROWS, COLS)
+                || dfs(r, c + 1, i + 1, board, word, positionSet, ROWS, COLS)
+                || dfs(r, c - 1, i + 1, board, word, positionSet, ROWS, COLS);
+        positionSet[r][c] = false;
+
+        return result;
+    }
 }
