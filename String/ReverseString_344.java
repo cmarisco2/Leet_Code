d4c59eee2ac6d2a7e5ac1b449e6d9ca6fb138612
Christopher Marisco <christopher.marisco@gmail.com>
2022-04-01 12:37:41 -0600

    add solution notes

--- String/ReverseString_344.java
+++ String/ReverseString_344.java
@@ -1,3 +1,29 @@
+//? Leetcode Problem 344: 'Reverse String'
+//? Difficulty: 'Easy'
+//? Description:
+// Write a function that reverses a string. The input string is given as an array of characters s.
+
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
+// //? Examples:
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+
+
+// Input: s = ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
+
+//* NOTE: These are Char[] not STRINGS. 
+//* -Due to immutability of strings in many programming languages, an efficient solution wouldn't be feasible if rebuilding a string occurred on each swap.
+
+
+//? Solution Approach: 2Ptr
+//* 1) Use Left and Right pointers that are at the start and end of array
+//* 2) Swap, While Left < Right
+
+//? Time Complexity: O(N)
+//? Space Complexity: O(1)
+
 class ReverseString_344{
     public void reverseString(char[] s) {
         // initialize left and right array pointers
