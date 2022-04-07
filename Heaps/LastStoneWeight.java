6d4687b2053328111015ab1a6ec682728c7324f0
Christopher Marisco <christopher.marisco@gmail.com>
2022-04-07 15:34:24 -0600

    add solution notes

--- Heaps/LastStoneWeight.java
+++ Heaps/LastStoneWeight.java
@@ -1,5 +1,32 @@
+//? Leetcode Problem 1046: Last Stone Weight
+//? Difficulty: 'Easy'
+//? Description: 
 
+// You are given an array of integers stones where stones[i]is the weight of the ith stone.
 
+// We are playing a game with the stones.On each turn,we choose the heaviest two stones and smash them together.Suppose the heaviest two stones have weights x and y with x<=y.The result of this smash is:
+
+// If x==y,both stones are destroyed,and If x!=y,the stone of weight x is destroyed,and the stone of weight y has new weight y-x.At the end of the game,there is at most one stone left.
+
+// Return the smallest possible weight of the left stone.If there are no stones left,return 0.
+
+//? Example:
+// Input: stones = [2,7,4,1,8,1]
+// Output: 1
+// Explanation: 
+// We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+// we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+// we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+// we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of the last stone.
+
+//? Solution Approach: Priority Queue
+//* 1) Create a MaxHeap PQ.
+//* 2) Add all elements of array into the queue.
+//* 3) While pq size > 1
+    //* dq twice, storing the values
+    //* insert the difference back into the pq.
+    //* pq will be of size 1 when the loop terminates.
+//* 4) return last value in the pq.
 
 import java.util.Collections;
 import java.util.PriorityQueue;
