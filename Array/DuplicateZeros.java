6f026de675edf8445c762639470428fffc05e570
Christopher Marisco <christopher.marisco@gmail.com>
2022-04-07 14:01:19 -0600

    Add Solution Notes

--- Array/DuplicateZeros.java
+++ Array/DuplicateZeros.java
@@ -1,7 +1,29 @@
+//? Leetcode Problem 1089: Duplicate Zeros
+//? Difficulty: 'Easy'
+//? Description:
+
+// Given a fixed-length integer array arr, duplicate each occurrence of zero, shifting the remaining elements to the right.
+
+// Note that elements beyond the length of the original array are not written. Do the above modifications to the input array in place and do not return anything.
+
+//? Examples:
+// Input: arr = [1,0,2,3,0,4,5,0]
+// Output: [1,0,0,2,3,0,0,4]
+// Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+
+//? Solution Approach:
+//* 1) Use Separate List<>
+//* 2) Copy All elements to new list except:
+        //* if elem == 0 -> copy it twice.
+//* 3) Copy new list back to old array ONLY for length of original array.
+
 class DuplicateZeros{
-    class Solution {
+
         public void duplicateZeros(int[] arr) {
+            //Separate List
             int[] dup = new int[arr.length];
+
+            //Copy Elements into new list, except copy 0 twice
             for (int i = 0, j = 0; j < arr.length && i < arr.length; i++, j++) {
                 if (arr[i] == 0) {
                     dup[j++] = arr[i];
@@ -11,10 +33,11 @@ class DuplicateZeros{
                 dup[j] = arr[i];
             }
 
+            //copy new list into old list
             int k = 0;
             for (int num : dup) {
                 arr[k++] = num;
             }
         }
-    }
+    
 }
