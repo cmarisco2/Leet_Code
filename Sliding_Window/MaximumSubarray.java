a220cdf2ea33e50c59a9d88af8663055c27b8cd9
Christopher Marisco <christopher.marisco@gmail.com>
2022-04-14 12:38:06 -0600

    Add solution notes

--- Sliding_Window/MaximumSubarray.java
+++ Sliding_Window/MaximumSubarray.java
@@ -1,4 +1,22 @@
+//? Leetcode Problem: 53 -- Maximum Subarray
+//? Difficulty: 'Easy'
+//? Description: 
 
+// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+// A subarray is a contiguous part of an array.
+
+//? Examples:
+
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: [4,-1,2,1] has the largest sum = 6.
+
+// Input: nums = [1]
+// Output: 1
+
+// Input: nums = [5,4,-1,7,8]
+// Output: 23
 
 //? Solution Notes: 
 //* Keep Running SUM of contigous elements
