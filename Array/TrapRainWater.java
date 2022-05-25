61235300bfbc5fcc1150d05fcd4a2f760feef91b
Christopher Marisco <christopher.marisco@gmail.com>
2022-05-25 13:42:22 -0600

    Add Solution Notes and a Test Case

--- Array/TrapRainWater.java
+++ Array/TrapRainWater.java
@@ -1,5 +1,45 @@
+import java.util.Arrays;
+
+//? Leetcode Problem 42: Trapping Rain Water
+//? Difficulty: 'Hard'
+//? Description: 
+// Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+//? Examples:
+//? Image: https://leetcode.com/problems/trapping-rain-water/
+
+// Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+// Output: 6
+// Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+
+// Input: height = [4,2,0,3,2,5]
+// Output: 9
+
+//? Solution Approach:
+//* Prefix/Suffix Array approach
+//* @ each given location:
+    //* Know maxLeft & maxRight heights
+    //* calculate the Units of water at this position
+
+//? Solution Shown:
+//* Sum += minimumHeight(maxLeft, maxRight) @ i - height[i]
+    //* if negative -> add 0.
+
+//* Need to build maxLeft[] and maxRight[] to know maxheights at a given element i (similar to Prefix Sum)
+
+//? Ex:
+//* heights =  [0,1,0,2,1,0,1,3,2,1,2,1]
+//* maxLeft =  [0,0,1,1,2,2,2,2,3,3,3,3]
+//* maxRight = [3,3,3,3,3,3,3,2,2,2,1,0]
+//* minLR  =   [0,0,1,1,2,2,2,2,2,2,1,0]
+//* tmpSum = minLR[i] - height[i] -> if >= 0, sum += tmpSum
+
+
+//? Time Complexity -> O(N)
+//? Space Complexity -> O(3*N) == O(N)
+
 public class TrapRainWater{
-    public int trap(int[] height) {
+    public static int trap(int[] height) {
         int sum = 0;
         int[] leftMax = new int[height.length];
         int[] rightMax = new int[height.length];
@@ -30,5 +70,11 @@ public class TrapRainWater{
                 sum += tempSum;
         }
         return sum;
+    }
+
+    public static void main(String[] args){
+        int[] heights = {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println("\nValley heights are: " + Arrays.toString(heights));
+        System.out.println("\nThe max amount of water trapped possible is: " + trap(heights) + "\n"); //6
     }
 }
