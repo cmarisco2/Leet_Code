d32e4a31c40b1917ae55f0508848ab62510a4396
Christopher Marisco <christopher.marisco@gmail.com>
2022-06-04 23:07:00 -0600

    add solution notes

--- Array/SearchInsertPosition.java
+++ Array/SearchInsertPosition.java
@@ -1,3 +1,27 @@
+//? Leetcode Problem 35: Search Insert Position
+//? Difficulty: 'Easy'
+//? Description:
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+//? Examples:
+// Input: nums = [1,3,5,6], target = 5
+// Output: 2
+
+// Input: nums = [1,3,5,6], target = 2
+// Output: 1
+
+// Input: nums = [1,3,5,6], target = 7
+// Output: 4
+
+//? Solution Approach: Binary Search
+//* CHANGE BASE CASE:
+    //* Old: if(hi < lo) return -1;
+    //* New: if(hi < lo) return lo;
+
+//? Time complexity: O(log(n))
+//? Space complexity: O(log(n)) number of stack frames?
 public class SearchInsertPosition{
     public int searchInsert(int[] nums, int target) {
         if (target > nums[nums.length - 1])
