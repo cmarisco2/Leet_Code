62587685ac4cd504dd45a4563e46e30209406e98
Christopher Marisco <christopher.marisco@gmail.com>
2022-06-08 18:10:45 -0600

    add no argument constructor. test pathTo(int v) in main()

--- Graphs/GraphStructures/DepthFirstPathsUndirected.java
+++ Graphs/GraphStructures/DepthFirstPathsUndirected.java
@@ -13,6 +13,14 @@ public class DepthFirstPathsUndirected{
         dfs(G, s);
     }
 
+    public DepthFirstPathsUndirected(UndirectedGraph G) {
+        marked = new boolean[G.getV()];
+        edgeTo = new int[G.getV()];
+        s = 0;
+
+        dfs(G, s);
+    }
+
     public boolean hasPathTo(int v){
         return marked[v];
     }
@@ -33,5 +41,28 @@ public class DepthFirstPathsUndirected{
         for(int i = v; i != this.s; i = edgeTo[i]) path.push(i);
         path.push(this.s);
         return path;
+    }
+
+    public static void main(String[] args) {
+        // Create Graph G, with 6 vertices and 8 edges
+        UndirectedGraph G = new UndirectedGraph(6);
+        G.addEdge(0, 1);
+        G.addEdge(0, 4);
+        G.addEdge(0, 2);
+        G.addEdge(1, 4);
+        G.addEdge(4, 2);
+        G.addEdge(4, 3);
+        G.addEdge(2, 3);
+        G.addEdge(2, 5);
+        G.printGraph();
+        System.out.println("\nNumber of Vertices: " + G.getV());
+        System.out.println("\nNumber of Edges: " + G.getE());
+
+        DepthFirstPathsUndirected path = new DepthFirstPathsUndirected(G);
+        int source = 4;
+        System.out.println("\nPath from " + source + ":\n");
+        for(int v : path.pathTo(source)){
+            System.out.print(v + " ");
+        }
     }
 }
