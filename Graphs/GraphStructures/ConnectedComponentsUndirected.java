9fd29b8d950ce98e3cdb39468d8d881e4591b7af
Christopher Marisco <christopher.marisco@gmail.com>
2022-06-09 14:13:11 -0600

    add test cases for cc'd

--- Graphs/GraphStructures/ConnectedComponentsUndirected.java
+++ Graphs/GraphStructures/ConnectedComponentsUndirected.java
@@ -43,4 +43,32 @@ public class ConnectedComponentsUndirected{
             }
         }
     }
+
+    public static void main(String[] args) {
+        // Create Graph G, with 6 vertices and 8 edges
+        UndirectedGraph G = new UndirectedGraph(6);
+        G.addEdge(0, 5);
+        G.addEdge(0, 3);
+        G.addEdge(3, 5);
+        G.addEdge(5, 4);
+        G.addEdge(1, 2);
+        G.printGraph();
+        System.out.println("\nNumber of Vertices: " + G.getV());
+        System.out.println("\nNumber of Edges: " + G.getE());
+
+        ConnectedComponentsUndirected cc = new ConnectedComponentsUndirected(G);
+        System.out.println("Number of Connected Components: " + cc.count());
+        int v = 3, w = 5, z = 2;
+        System.out.println("Component " + v + " is connected to " + w + "?:\n");
+        System.out.println(cc.connected(v, w));
+        System.out.println();
+        System.out.println("id of " + v + " is: " + cc.id(v));
+        System.out.println("id of " + w + " is: " + cc.id(w));
+        System.out.println();
+        System.out.println("Component " + v + " is connected to " + z + "?:\n");
+        System.out.println(cc.connected(v, z));
+        System.out.println();
+        System.out.println("id of " + v + " is: " + cc.id(v));
+        System.out.println("id of " + z + " is: " + cc.id(z));
+    }
 }
